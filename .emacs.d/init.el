dd9cb100c55ec2f0f1f3d7b363bde01e08b37465
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2015-02-23 11:14:23 +0900

    複数の define-key -> bind-keys

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -128,15 +128,15 @@
                (flycheck-mode -1) ; to cancel global flycheck mode
                (ghc-init)))
   :config
-  (progn
-    (define-key haskell-mode-map (kbd "C-x C-d") nil)
-    (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-    (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
-    (define-key haskell-mode-map (kbd "C-c C-b") 'haskell-interactive-switch)
-    ;; (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
-    ;; (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
-    (define-key haskell-mode-map (kbd "C-c M-.") nil)
-    (define-key haskell-mode-map (kbd "C-c C-d") nil)))
+  (bind-keys :map haskell-mode-map
+             ("C-x C-d" . nil)
+             ("C-c C-z" . haskell-interactive-switch)
+             ("C-c C-l" . haskell-process-load-file)
+             ("C-c C-b" . haskell-interactive-switch)
+             ;; ("C-c C-t" . haskell-process-do-type)
+             ;; ("C-c C-i" . haskell-process-do-info)
+             ("C-c M-." . nil)
+             ("C-c C-d" . nil)))
 
 (use-package helm
   :ensure t
