732ec058414b9dca5f8c0fd40bf692b83156db56
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2016-07-22 20:10:22 +0900

    Emacs 安全なカスタムテーマを記憶する

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -349,6 +349,9 @@ Optionally takes FRAME for its target and works on current frame if nothing give
  '(before-save-hook (quote (delete-trailing-whitespace)))
  '(column-number-mode t)
  '(confirm-kill-emacs (quote y-or-n-p))
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(dired-dwim-target t)
  '(haskell-process-type (quote stack-ghci))
  '(indent-tabs-mode nil)
