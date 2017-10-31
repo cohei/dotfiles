4fa8c8cdee7af81701f240ff87c8c77b452c88db
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2017-10-31 23:18:04 +0900

    Emacs: do not use customize about kill confirmation

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,6 +14,8 @@
 (when (memq window-system '(ns mac))
   (setq ring-bell-function 'ignore))
 
+(setq confirm-kill-emacs 'y-or-n-p)
+
 ;;;; Language
 
 (set-language-environment 'Japanese)
@@ -386,12 +388,14 @@ Optionally takes FRAME for its target and works on current frame if nothing give
 (use-package yaml-mode
   :ensure t)
 
+;;; Avoid to write `package-selected-packages` in init.el
+;; (load (setq custom-file (expand-file-name "custom.el" user-emacs-directory)))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(confirm-kill-emacs (quote y-or-n-p))
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
