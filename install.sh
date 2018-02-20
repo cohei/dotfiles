1f42d46da0178162a34020a3dde475ca443227b3
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2018-02-20 23:30:27 +0900

    Files to be installed are in `home` directory

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 set -eux
 
 repository=~/.dotfiles
+dotfiles_dir="$repository/home"
 
 download() {
     git clone https://github.com/cohei/dotfiles.git $repository
@@ -11,39 +12,23 @@ download() {
 initialize() {
     # Git remembers only permissions about executability.
     # ghci reads .ghci when it does not allow to be written from other users.
-    chmod 644 $repository/.ghci
+    chmod 644 $dotfiles_dir/.ghci
 }
 
 link() {
     dist=~
 
-    targets=(
-        .Brewfile
-        .bash_aliases
-        .bash_profile
-        .bashrc
-        .bundle/config
-        .config/git/ignore
-        .emacs.d/init.el
-        .ghci
-        .gitconfig
-        .pryrc
-        .rspec
-        .ssh/config
-        bin/dotall
-        bin/gem-uninstall-all
-        bin/grepr
-        bin/shell_expansion
-    )
-
-    for target in "${targets[@]}"; do
+    while read -r target_with_dot; do
+	target=${target_with_dot#*/}
+	
         dir=$(dirname "$target")
         [ -d "$dist/$dir" ] || mkdir -p "$dist/$dir"
 
-        ln --symbolic --relative --backup "$repository/$target" "$dist/$target" || \
-        ln --symbolic            --backup "$repository/$target" "$dist/$target" || \
-        ln -s -f                          "$repository/$target" "$dist/$target"
-    done
+        ln --symbolic --relative --backup "$dotfiles_dir/$target" "$dist/$target" || \
+        ln --symbolic            --backup "$dotfiles_dir/$target" "$dist/$target" || \
+        ln -s -f                          "$dotfiles_dir/$target" "$dist/$target"
+
+    done < <(cd $dotfiles_dir && find . -type f)
 }
 
 [ -d $repository ] || download
