c96d166188e683a3e9f76b84a739cb5b0e11411c
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2018-02-20 21:42:48 +0900

    install.sh: generalize directory making

--- install.sh
+++ install.sh
@@ -36,13 +36,10 @@ link() {
         bin/shell_expansion
     )
 
-    [ -d $dist/.bundle ]     || mkdir    $dist/.bundle
-    [ -d $dist/.config/git ] || mkdir -p $dist/.config/git
-    [ -d $dist/.emacs.d ]    || mkdir    $dist/.emacs.d
-    [ -d $dist/.ssh ]        || mkdir    $dist/.ssh
-    [ -d $dist/bin ]         || mkdir    $dist/bin
-
     for target in "${targets[@]}"; do
+        dir=$(dirname "$target")
+        [ -d "$dist/$dir" ] || mkdir -p "$dist/$dir"
+
         ln --symbolic --relative --backup "$repository/$target" "$dist/$target" || \
         ln --symbolic            --backup "$repository/$target" "$dist/$target" || \
         ln -s -f                          "$repository/$target" "$dist/$target"
