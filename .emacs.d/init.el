2c177985063911947bf4550c7146c03c2c265350
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2018-03-07 13:45:01 +0900

    Emacs: magit-status buffer name changed

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -297,7 +297,7 @@ Optionally takes FRAME for its target and works on current frame if nothing give
   (popwin-mode 1)
   (push '("*Warnings*" :height 0.3) popwin:special-display-config)
   (push '("*Buffer List*" :height 0.3) popwin:special-display-config)
-  (push '("\\*magit" :regexp t :height 0.5) popwin:special-display-config)
+  (push '("magit:" :regexp t :height 0.5) popwin:special-display-config)
   (push '("\\*helm" :regexp t) popwin:special-display-config)
   (push '("*GHC Info*" :height 10) popwin:special-display-config)
   (push '(" *undo-tree*" :width 0.1 :position right) popwin:special-display-config)
