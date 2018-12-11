535ce6dd2491c5f6b691165a7ba71c42a9b76f70
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2018-12-11 13:02:05 +0900

    Nix: declarative package management

---
+++ home/.config/nixpkgs/config.nix
@@ -0,0 +1,42 @@
+{
+  packageOverrides = pkgs:
+    with pkgs; {
+      myPackages = buildEnv {
+        name = "my-packages";
+        paths = [
+          bashCompletion
+          bashInteractive
+          cacert # not to cause SSL error
+          cloc
+          coreutils
+          direnv
+          emacs
+          fzf
+          ghq
+          git
+          gitAndTools.diff-so-fancy
+          gitAndTools.hub
+          haskellPackages.ghcid
+          haskellPackages.hlint
+          haskellPackages.hoogle
+          haskellPackages.hpack
+          haskellPackages.stylish-haskell
+          hledger
+          ipfs
+          jq
+          nix
+          nix-bash-completions
+          pwgen
+          sbt
+          shellcheck
+          stack
+          terraform
+          tmux
+          tree
+          watch
+          wget
+          xz # for ghcup
+        ];
+      };
+    };
+}
