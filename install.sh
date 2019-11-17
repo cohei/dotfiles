c25e358c4d65c58a4cd9a1a8b8f9181a631ac5d7
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2019-11-17 15:56:24 +0900

    Install in XDG_DATA_HOME

--- install.sh
+++ install.sh
@@ -2,17 +2,17 @@
 
 set -eux
 
-repository=~/.dotfiles
+repository=${XDG_DATA_HOME:-$HOME/.local/share}/dotfiles
 dotfiles_dir="$repository/home"
 
 download() {
-    git clone https://github.com/cohei/dotfiles.git $repository
+    git clone https://github.com/cohei/dotfiles.git "$repository"
 }
 
 initialize() {
     # Git remembers only permissions about executability.
     # ghci reads .ghci when it does not allow to be written from other users.
-    chmod 644 $dotfiles_dir/.ghci
+    chmod 644 "$dotfiles_dir/.ghci"
 }
 
 link() {
@@ -28,9 +28,9 @@ link() {
         ln --symbolic            --backup "$dotfiles_dir/$target" "$dist/$target" || \
         ln -s -f                          "$dotfiles_dir/$target" "$dist/$target"
 
-    done < <(cd $dotfiles_dir && find . -type f)
+    done < <(cd "$dotfiles_dir" && find . -type f)
 }
 
-[ -d $repository ] || download
+[ -d "$repository" ] || download
 initialize
 link
