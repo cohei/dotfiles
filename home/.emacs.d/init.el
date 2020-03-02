c5391ab500030c4c5a74f232029a6ee31f562686
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2020-03-03 01:13:52 +0900

    Emacs: rename `my/maximize-and-split`
    What actually do is fullscreen-ing.

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -201,18 +201,18 @@
 (use-package frame
   :if (memq window-system '(ns x mac)) ; cocoa, carbon -> mac, terminal -> nil, X -> x
   :init
-  (defun my/maximize-and-split (&optional frame)
-    "Maximize the window and split it horizontally into two buffers.
+  (defun my/fullscreen-and-split (&optional frame)
+    "Fullscreen the window and split it horizontally into two buffers.
 Optionally takes FRAME for its target and works on current frame if nothing given."
     (if frame
         (select-window (frame-root-window frame)))
     (toggle-frame-fullscreen)
     (split-window-horizontally))
   ;; when make-frame
-  (add-hook 'after-make-frame-functions #'my/maximize-and-split)
+  (add-hook 'after-make-frame-functions #'my/fullscreen-and-split)
   :hook
   ;; when startup
-  (window-setup . my/maximize-and-split))
+  (window-setup . my/fullscreen-and-split))
 
 (use-package flycheck
   :ensure t
