b5bc35db9ff3576e1c61ea0b33e2d24ffcfece17
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2020-03-05 17:34:04 +0900

    Nix: home-manager manages packages

--- home/.config/nixpkgs/home.nix
+++ home/.config/nixpkgs/home.nix
@@ -13,4 +13,66 @@
   # the Home Manager release notes for a list of state version
   # changes in each release.
   home.stateVersion = "20.03";
+
+  home.packages =
+    with pkgs;
+    let
+      enhancd = import ./enhancd.nix { inherit stdenv fetchFromGitHub runtimeShell; };
+    in
+      [
+        bashCompletion
+        bashInteractive_5
+        bat
+        cloc
+        cmake
+        coreutils
+        direnv
+        dust
+        emacs
+        enhancd
+        exa
+        fd
+        ffmpeg
+        fish
+        fswatch
+        fzf
+        ghq
+        git
+        gitAndTools.diff-so-fancy
+        gitAndTools.gh
+        gitAndTools.hub
+        google-cloud-sdk
+        # haskellPackages.cabal-fmt # compile error
+        haskellPackages.ghcid
+        # haskellPackages.hadolint # broken
+        haskellPackages.hlint
+        haskellPackages.hoogle
+        haskellPackages.hpack
+        haskellPackages.stylish-haskell
+        # haskellPackages.unused # broken
+        hledger
+        icdiff
+        ipfs
+        jl
+        jq
+        nix-bash-completions
+        nkf
+        parallel
+        procs
+        pstree
+        pwgen
+        sbt
+        shellcheck
+        skktools
+        solargraph
+        stack
+        starship
+        terminal-notifier
+        terraform_0_12
+        tmux
+        tree
+        watch
+        wget
+        ytop
+      ];
 }
