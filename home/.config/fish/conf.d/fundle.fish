d040d680e731645dfcb529b9216a1945ded78369
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2020-07-07 10:44:26 +0900

    Fish: z

--- home/.config/fish/conf.d/fundle.fish
+++ home/.config/fish/conf.d/fundle.fish
@@ -2,6 +2,7 @@ if not functions --query fundle
     eval (curl --silent --fail --location https://git.io/fundle-install)
 end
 
+fundle plugin 'jethrokuan/z'
 fundle plugin 'jorgebucaran/fish-bax'
 fundle init
 
