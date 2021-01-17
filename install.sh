a1ba32e08c695b4b11596e72ae9aa6774945511b
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2021-01-18 04:34:47 +0900

    install.sh: no longer needs local repository

--- install.sh
+++ install.sh
@@ -2,22 +2,14 @@
 
 set -eux
 
-repository=$HOME/.local/share/dotfiles
-
-download() {
-    nix-shell --packages git --command 'git clone https://github.com/cohei/dotfiles.git '"$repository"
-}
-
 ensure_nix_flakes() {
     nix-env --install --attr nixpkgs.nixFlakes
 }
 
 switch() {
     nix --experimental-features 'nix-command flakes' shell nixpkgs#git \
-        --command nix --experimental-features 'nix-command flakes' run "$repository#switch"
+        --command nix --experimental-features 'nix-command flakes' run 'github:cohei/dotfiles#switch'
 }
-
-[ -d "$repository" ] || download
 
 ensure_nix_flakes
 switch
