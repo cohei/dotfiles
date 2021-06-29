8e4e348d879eb29d498ceedb910b5fbec8f26615
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2021-06-29 23:15:01 +0900

    Nix: state version 21.05
    https://github.com/nix-community/home-manager/issues/2073

--- flake.nix
+++ flake.nix
@@ -23,6 +23,15 @@
           configuration = ./home.nix;
           inherit system username;
           homeDirectory = homeDirectory (isDarwin system) username;
+          # This value determines the Home Manager release that your
+          # configuration is compatible with. This helps avoid breakage
+          # when a new Home Manager release introduces backwards
+          # incompatible changes.
+          #
+          # You can update Home Manager without changing this value. See
+          # the Home Manager release notes for a list of state version
+          # changes in each release.
+          stateVersion = "21.05";
         };
     in
       utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin" "aarch64-linux"] (system: {
