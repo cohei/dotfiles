6e953db4c5014bf3f2544058c39724ad128f4df7
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2021-07-24 02:09:35 +0900

    Nix: simplify nixpkgs URL

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My Home";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
