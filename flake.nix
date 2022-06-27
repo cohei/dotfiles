c20a51f3a7484ee785b2e63a76d528596f6bfb7f
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2022-06-28 03:40:11 +0900

    Nix: refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -14,36 +14,38 @@
 
   outputs = { self, nixpkgs, home-manager, utils }:
     let
-      isDarwin = system: (import nixpkgs { inherit system; }).stdenv.isDarwin;
-
-      homeDirectory = isDarwin: username: "/${if isDarwin then "Users" else "home"}/${username}";
-
       modules =
         builtins.map (f: ./module + ("/" + f)) (builtins.attrNames (builtins.readDir ./module));
-
-      homeManagerConfiguration = system: username:
-        home-manager.lib.homeManagerConfiguration {
-          configuration = ./home.nix;
-          inherit system username;
-          homeDirectory = homeDirectory (isDarwin system) username;
-          extraModules = modules;
-          # This value determines the Home Manager release that your
-          # configuration is compatible with. This helps avoid breakage
-          # when a new Home Manager release introduces backwards
-          # incompatible changes.
-          #
-          # You can update Home Manager without changing this value. See
-          # the Home Manager release notes for a list of state version
-          # changes in each release.
-          stateVersion = "22.05";
-        };
     in
-      utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"] (system: {
-        apps.home-manager = utils.lib.mkApp { drv = home-manager.defaultPackage.${system}; };
+      utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"] (system:
+        let
+          isDarwin = (import nixpkgs { inherit system; }).stdenv.isDarwin;
 
-        packages.homeConfigurations = {
-          "root@testcontainer" = homeManagerConfiguration system "root";
-          hoge = homeManagerConfiguration system "hoge";
-        };
-      });
+          homeDirectory = username: "/${if isDarwin then "Users" else "home"}/${username}";
+
+          homeManagerConfiguration = username:
+            home-manager.lib.homeManagerConfiguration {
+              configuration = ./home.nix;
+              inherit system username;
+              homeDirectory = homeDirectory username;
+              extraModules = modules;
+              # This value determines the Home Manager release that your
+              # configuration is compatible with. This helps avoid breakage
+              # when a new Home Manager release introduces backwards
+              # incompatible changes.
+              #
+              # You can update Home Manager without changing this value. See
+              # the Home Manager release notes for a list of state version
+              # changes in each release.
+              stateVersion = "22.05";
+            };
+
+        in {
+          apps.home-manager = utils.lib.mkApp { drv = home-manager.defaultPackage.${system}; };
+
+          packages.homeConfigurations = {
+            "root@testcontainer" = homeManagerConfiguration "root";
+            hoge = homeManagerConfiguration "hoge";
+          };
+        });
 }
