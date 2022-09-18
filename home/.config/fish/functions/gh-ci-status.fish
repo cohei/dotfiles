b4fe14222f2fb8b68f8bd777025dcae05ec6a010
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2022-09-18 13:47:27 +0900

    gh: use fish function for `ci-status`

---
+++ home/.config/fish/functions/gh-ci-status.fish
@@ -0,0 +1,7 @@
+function gh-ci-status
+    set --local revision (git rev-parse $argv[1])
+    set --local header 'Accept: application/vnd.github.antiope-preview+json'
+
+    gh api --header $header repos/:owner/:repo/commits/{$revision}/check-runs | \
+        jq --raw-output '.check_runs[] | "\(.conclusion)  \(.name)  \(.html_url)"'
+end
