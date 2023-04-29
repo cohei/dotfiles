467fd3e5c64257e2c5dae93e4aa95bfc17fdead4
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2023-04-29 23:43:50 +0900

    flake.nix: omit inputs which are registered in flake registry

--- flake.nix
+++ flake.nix
@@ -2,17 +2,10 @@
   description = "My Home";
 
   inputs = {
-    nixpkgs.url = "nixpkgs";
-
-    home-manager = {
-      url = "home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    utils.url = "flake-utils";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, utils }:
+  outputs = { self, nixpkgs, home-manager, flake-utils }:
     let
       modules =
         builtins.map (f: ./module + ("/" + f)) (builtins.attrNames (builtins.readDir ./module));
@@ -26,10 +19,10 @@
           ] ++ modules;
         };
     in
-      utils.lib.eachDefaultSystem (system: {
-        apps.home-manager = utils.lib.mkApp { drv = home-manager.defaultPackage.${system}; };
+      flake-utils.lib.eachDefaultSystem (system: {
+        apps.home-manager = flake-utils.lib.mkApp { drv = home-manager.defaultPackage.${system}; };
       }) // {
-        homeConfigurations = with utils.lib; {
+        homeConfigurations = with flake-utils.lib; {
           "root:${system.x86_64-linux}" = homeManagerConfiguration "root" system.x86_64-linux;
           "root:${system.aarch64-linux}" = homeManagerConfiguration "root" system.aarch64-linux;
           "cohei:${system.x86_64-darwin}" = homeManagerConfiguration "cohei" system.x86_64-darwin;
