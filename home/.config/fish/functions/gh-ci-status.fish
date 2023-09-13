92a0ce4a49274330c9734a917cfaefaeebfed94d
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2023-09-13 14:58:22 +0900

    gh-ci-status: add --open option

--- home/.config/fish/functions/gh-ci-status.fish
+++ home/.config/fish/functions/gh-ci-status.fish
@@ -1,7 +1,15 @@
 function gh-ci-status
+    argparse 'open' -- $argv
+    or return
+
     set --local revision (git rev-parse $argv[1])
     set --local header 'Accept: application/vnd.github.antiope-preview+json'
+    set --local json (gh api --header $header repos/:owner/:repo/commits/{$revision}/check-runs)
+    set --local result (echo $json | jq --raw-output '.check_runs[] | "\(.conclusion)\t\(.name)\t\(.html_url)"')
 
-    gh api --header $header repos/:owner/:repo/commits/{$revision}/check-runs | \
-        jq --raw-output '.check_runs[] | "\(.conclusion)\t\(.name)\t\(.html_url)"'
+    if set --query _flag_open
+        open (string split -f 3 \t $result)
+    else
+        echo $result
+    end
 end
