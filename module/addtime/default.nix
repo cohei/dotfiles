cf342b19b31b81f6affaf4701fdd59cbe27ad423
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2023-11-22 00:46:39 +0900

    Nix: addtime: do not have to use overlay

--- module/addtime/default.nix
+++ module/addtime/default.nix
@@ -1,13 +1,7 @@
 { pkgs, ... }:
 
 {
-  nixpkgs.overlays = [
-    (self: super: {
-      addtime = super.callPackage ./derivation.nix {};
-    })
-  ];
-
   home.packages = [
-    pkgs.addtime
+    (pkgs.callPackage ./derivation.nix {})
   ];
 }
