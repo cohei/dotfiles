705e48da8dba8036f8110d27e336c03495d06048
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2023-12-01 15:24:46 +0900

    Emacs: reduce markdown-mode settings

--- module/emacs/init.el
+++ module/emacs/init.el
@@ -424,11 +424,8 @@
 
 (leaf markdown-mode
   :straight t
-  :commands markdown-mode gfm-mode
   :mode
-  ("README\\.md\\'" . gfm-mode)
-  ("\\.md\\'" . markdown-mode)
-  ("\\.markdown\\'" . markdown-mode))
+  ("README\\.md\\'" . gfm-mode))
 
 (leaf marginalia
   :straight t
