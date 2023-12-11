783ca7c99347b280d1a8291eb6a4a82c0683050b
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2023-12-11 23:22:17 +0900

    gh-ci-status: now `gh api` has `jq` option

--- module/git/gh-ci-status.fish
+++ module/git/gh-ci-status.fish
@@ -3,9 +3,7 @@ function gh-ci-status
     or return
 
     set --local revision (git rev-parse $argv[1])
-    set --local header 'Accept: application/vnd.github.antiope-preview+json'
-    set --local json (gh api --header $header repos/:owner/:repo/commits/{$revision}/check-runs)
-    set --local result (echo $json | jq --raw-output '.check_runs[] | "\(.conclusion)\t\(.name)\t\(.html_url)"')
+    set --local result (gh api repos/:owner/:repo/commits/{$revision}/check-runs --jq '.check_runs[] | "\(.conclusion)\t\(.name)\t\(.html_url)"')
 
     if set --query _flag_open
         open (string split -f 3 \t $result)
