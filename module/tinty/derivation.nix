a3e8abdb61518cb78836be56aab5d4c6812b5d7f
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2024-03-31 13:13:25 +0900

    Tinty 0.11.0

--- module/tinty/derivation.nix
+++ module/tinty/derivation.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "tinty";
-  version = "0.10.1";
+  version = "0.11.0";
 
   src = fetchFromGitHub {
     owner = "tinted-theming";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-7E/ApLoQLD+Gi9UqJDuSsx1p1aGyjxIH/CKfmnuwlsk=";
+    hash = "sha256-wUOnhgTnyLqkcc1+nE8fTUTkVj8yhsSN4JUFpW5JLAk=";
   };
 
-  cargoHash = "sha256-XuB8wDscn6eJLHQxPPhv9GaKpymK19rkKUCP1bSNma0=";
+  cargoHash = "sha256-fvCyMvtrk4c0E5OF0AC394kCUSWs9SlHGwmO8i3VrbY=";
 
   # Tests faild, maybe due to homeless-shelter
   doCheck = false;
