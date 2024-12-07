b0a9987391a7cfe56f995a6d6213a17560435d72
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2024-12-08 04:38:39 +0900

    Nix: per-system home configurations

--- flake.nix
+++ flake.nix
@@ -14,43 +14,38 @@
   };
 
   outputs = { self, nixpkgs, nixpkgs-unfree, home-manager, flake-utils, mac-app-util }:
-    let
-      modules =
-        builtins.map (f: ./module + ("/" + f)) (builtins.attrNames (builtins.readDir ./module));
+    flake-utils.lib.eachDefaultSystem (system: {
+      apps = rec {
+        default = install;
 
-      homeManagerConfiguration = username: system:
-        home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.${system};
-          modules = [ ./home.nix ] ++ modules;
-          extraSpecialArgs = {
-            inherit username mac-app-util;
-            unfree = nixpkgs-unfree.legacyPackages.${system};
-            isDarwin =
-              with flake-utils.lib.system;
-              system == x86_64-darwin || system == aarch64-darwin;
+        install = flake-utils.lib.mkApp {
+          drv = nixpkgs.legacyPackages.${system}.writeShellApplication {
+            name = "install";
+            runtimeInputs = [ home-manager.packages.${system}.default ];
+            text = ''
+              home-manager switch --flake "''${1:-github:cohei/dotfiles}"
+            '';
           };
-        };
-    in
-      flake-utils.lib.eachDefaultSystem (system: {
-        apps = rec {
-          default = install;
-
-          install = flake-utils.lib.mkApp {
-            drv = nixpkgs.legacyPackages.${system}.writeShellApplication {
-              name = "install";
-              runtimeInputs = [ home-manager.packages.${system}.default ];
-              text = ''
-                home-manager switch --flake "''${1:-github:cohei/dotfiles}#''${USER}:${system}"
-              '';
-            };
-          };
-        };
-      }) // {
-        homeConfigurations = with flake-utils.lib.system; {
-          "root:${x86_64-linux}" = homeManagerConfiguration "root" x86_64-linux;
-          "root:${aarch64-linux}" = homeManagerConfiguration "root" aarch64-linux;
-          "cohei:${x86_64-darwin}" = homeManagerConfiguration "cohei" x86_64-darwin;
-          "cohei:${aarch64-darwin}" = homeManagerConfiguration "cohei" aarch64-darwin;
         };
       };
+
+      legacyPackages.homeConfigurations =
+        let
+          modules =
+            builtins.map (f: ./module + ("/" + f)) (builtins.attrNames (builtins.readDir ./module));
+        in
+          nixpkgs.lib.attrsets.genAttrs [ "root" "cohei" ] (username:
+            home-manager.lib.homeManagerConfiguration {
+              pkgs = nixpkgs.legacyPackages.${system};
+              modules = [ ./home.nix ] ++ modules;
+              extraSpecialArgs = {
+                inherit username mac-app-util;
+                unfree = nixpkgs-unfree.legacyPackages.${system};
+                isDarwin =
+                  with flake-utils.lib.system;
+                  system == x86_64-darwin || system == aarch64-darwin;
+              };
+            }
+          );
+    });
 }
