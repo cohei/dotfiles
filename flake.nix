1ceffd2e7a710266493d2efb0152779cda454bed
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2024-12-08 05:32:45 +0900

    flake-parts

--- flake.nix
+++ flake.nix
@@ -8,43 +8,45 @@
     };
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     darwin-systems.url = "github:nix-systems/default-darwin";
-    mac-app-util = {
-      url = "github:hraban/mac-app-util";
-      inputs.flake-utils.follows = "flake-utils";
-    };
+    mac-app-util.url = "github:hraban/mac-app-util";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unfree, home-manager, flake-utils, darwin-systems, mac-app-util }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      apps = {
-        default = self.apps.${system}.install;
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unfree, home-manager, systems, flake-parts, darwin-systems, mac-app-util }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import systems;
+      perSystem = { pkgs, lib, inputs', self', system, ... }: {
+        apps = {
+          default = self'.apps.install;
 
-        install = flake-utils.lib.mkApp {
-          drv = nixpkgs.legacyPackages.${system}.writeShellApplication {
-            name = "install";
-            runtimeInputs = [ home-manager.packages.${system}.default ];
-            text = ''
-              home-manager switch --flake "''${1:-github:cohei/dotfiles}"
-            '';
+          install = {
+            type = "app";
+            program = lib.getExe (pkgs.writeShellApplication {
+              name = "install";
+              runtimeInputs = [ inputs'.home-manager.packages.default ];
+              text = ''
+                home-manager switch --flake "''${1:-github:cohei/dotfiles}"
+              '';
+            });
           };
         };
-      };
 
-      legacyPackages.homeConfigurations =
-        let
-          modules =
-            builtins.map (f: ./module + ("/" + f)) (builtins.attrNames (builtins.readDir ./module));
-        in
-          nixpkgs.lib.attrsets.genAttrs [ "root" "cohei" ] (username:
-            home-manager.lib.homeManagerConfiguration {
-              pkgs = nixpkgs.legacyPackages.${system};
-              modules = [ ./home.nix ] ++ modules;
-              extraSpecialArgs = {
-                inherit username mac-app-util;
-                unfree = nixpkgs-unfree.legacyPackages.${system};
-                isDarwin = builtins.elem system (import darwin-systems);
-              };
-            }
-          );
-    });
+        legacyPackages.homeConfigurations =
+          let
+            modules =
+              builtins.map (f: ./module + ("/" + f)) (builtins.attrNames (builtins.readDir ./module));
+          in
+            nixpkgs.lib.attrsets.genAttrs [ "root" "cohei" ] (username:
+              home-manager.lib.homeManagerConfiguration {
+                inherit pkgs;
+                modules = [ ./home.nix ] ++ modules;
+                extraSpecialArgs = {
+                  inherit username mac-app-util;
+                  unfree = inputs'.nixpkgs-unfree.legacyPackages;
+                  isDarwin = builtins.elem system (import darwin-systems);
+                };
+              }
+            );
+
+      };
+    };
 }
