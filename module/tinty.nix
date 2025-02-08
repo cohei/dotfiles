0884b692f9a02dc73d1fef4105687ea9aa01c9cf
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2025-02-08 23:25:12 +0900

    Install tinty from nixpkgs

--- module/tinty.nix
+++ module/tinty.nix
@@ -1,9 +1,7 @@
 { pkgs, ... }:
 
 {
-  home.packages = [
-    (pkgs.callPackage ./derivation.nix {})
-  ];
+  home.packages = [ pkgs.tinty ];
 
   programs.fish.interactiveShellInit = ''
     tinty apply base16-solarized-dark
