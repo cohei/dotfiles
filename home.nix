c7b893117cb1ab7f21339b8a476c000ee90ee905
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2025-03-22 02:16:34 +0900

    Docker: COMPOSE_BAKE

--- home.nix
+++ home.nix
@@ -64,6 +64,7 @@
   home.language.base = "ja_JP.UTF-8";
   home.sessionPath = [ "$HOME/.local/bin" "$HOME/.docker/bin" ];
   home.sessionVariables = {
+    COMPOSE_BAKE = "true";
     GHCUP_USE_XDG_DIRS = "yes";
     LESS = "--LONG-PROMPT --RAW-CONTROL-CHARS --quit-if-one-screen --no-init";
   };
