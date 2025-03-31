caf620a595ec6303a908f66ac7d8ab9d47daa567
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2025-03-31 16:09:08 +0900

    touch-sudo darwin

--- module/darwin/default.nix
+++ module/darwin/default.nix
@@ -7,11 +7,21 @@ lib.attrsets.optionalAttrs isDarwin {
     home.homeDirectory = lib.mkForce "/Users/${config.home.username}";
 
     home.packages =
-      with pkgs; [
-        unfree.appcleaner
-        mas
-        (callPackage ./clean-links.nix {})
-      ];
+      let
+        touch-sudo = pkgs.writeShellApplication {
+          name = "touch-sudo";
+          runtimeInputs = [ pkgs.gnused ];
+          text = ''
+            sed --in-place=.bak '2i auth       sufficient     pam_tid.so' /etc/pam.d/sudo
+          '';
+        };
+      in
+        with pkgs; [
+          unfree.appcleaner
+          mas
+          (callPackage ./clean-links.nix {})
+          touch-sudo
+        ];
 
     home.file = {
       ".Brewfile".source = ./.Brewfile;
--- module/my-commands.nix
+++ module/my-commands.nix
@@ -23,14 +23,7 @@ let
       rm "$tempfile"
     '';
   };
-  touch-sudo = pkgs.writeShellApplication {
-    name = "touch-sudo";
-    runtimeInputs = [ pkgs.gnused ];
-    text = ''
-      sed --in-place=.bak '2i auth       sufficient     pam_tid.so' /etc/pam.d/sudo
-    '';
-  };
 in
 {
-  home.packages = [ dotall shell-expansion touch-sudo ];
+  home.packages = [ dotall shell-expansion ];
 }
